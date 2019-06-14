5c14551ed02f3fdc3343dfdaa2761387612325b1
kellyodion <kodion@bren.ucsb.edu>
2019-06-13 22:48:12 -0400

    all functions accounted for in vignette

--- R/crop_loss.R
+++ R/crop_loss.R
@@ -15,29 +15,12 @@ crop_loss = function(precip, temp) {
   data_frame = merge(precip, temp, by = "Date") %>%
     rename(Precipitation = "Value.x", Precipitation_Anomaly = "Anomaly.x", Temperature = "Value.y", Temperature_Anomaly = "Anomaly.y")
   drought_ratio = min(data_frame$Precipitation/data_frame$Temperature)
- wet_ratio = max(data_frame$Precipitation/data_frame$Temperature)
+  wet_ratio = max(data_frame$Precipitation/data_frame$Temperature)
 
   return(list(drought = drought_ratio,
               wet = wet_ratio))
 
 }
 
-catch_fish <- function(price, catch, plot = F) {
 
-  most_common_fish = list(colnames(catch), rownames(catch)[apply(catch, 2, which.max)])
-  total_revenue = sum(price[,1]* catch)
-  loc_revenue = price[,1] * catch
-  loc_revenue = colSums(loc_revenue)
-  loc_revenue_df = data.frame(loc_revenue)
-  loc_revenue_df$location = c(rownames(loc_revenue_df))
-  if (plot) {                                           #create plot if user selects true
-    lb = sprintf("Total Revenue is $%d", total_revenue) #display total revenue
-    p = ggplot(loc_revenue_df, aes(location, loc_revenue, fill=location))+
-      geom_col()+
-      labs(y="Revenue ($)")+annotate("text", x=2, y=30000, label=lb, col="black") +
-      theme_classic() +
-      scale_fill_manual(values = c("slategrey","skyblue", "#00AFBB", "cornflowerblue", "navy"))
-  }
-  else p=NULL #no plot if user selects false
-  return(list(MostFrequent = most_common_fish, TotalRevenue = total_revenue, Revenue_by_Location = loc_revenue_df, plot = p)) #return specified outputs
-}
+
--- tests/testthat/test-crop_loss.R
+++ tests/testthat/test-crop_loss.R
@@ -7,7 +7,7 @@ library(testthat)
 
 test_that("ratio_is_positive", {
   precip =
-      data.frame(
+    data.frame(
       Date = c(2002,2003, 2004, 2005),
       Precipitation = c(1:4),
       Precipitation_Anomaly = c(1:4))
